452026a865279027361bc9a5509c3aa3f2a92998
joabe <joalokk@gmail.com>
2024-08-19 09:08:41 -0300

    agora é só deixar bonito

--- ProjetoAdiministracao/CadastroFuncionarios.xaml.cs
+++ ProjetoAdiministracao/CadastroFuncionarios.xaml.cs
@@ -7,71 +7,105 @@ namespace ProjetoAdiministracao;
 
 public partial class CadastroFuncionarios : ContentPage
 {
-    public Cliente cliente;
-    public ClienteControle clienteControle;
-    
-    public CadastroFuncionarios()
+  public Cliente cliente;
+  public ClienteControle clienteControle;
+
+  public CadastroFuncionarios()
+  {
+    InitializeComponent();
+    cliente = new Cliente();
+    clienteControle = new ClienteControle();
+  }
+
+  //---------------------------------------------------------------------------------------------------------------------\\
+
+  private void Apagar(object sender, EventArgs e)
+  {
+    // Handle the trash button click event
+    NameEntry.Text = string.Empty;
+    CPFEntry.Text = string.Empty;
+    RGEntry.Text = string.Empty;
+    HorarioEntry.Text = string.Empty;
+    SalarioEntry.Text = string.Empty;
+
+  }
+
+  private void Excluir(object sender, EventArgs e)
+  {
+    FrameAviso.IsVisible = true;
+  }
+
+  private void Certeza(object sender, EventArgs e)
+  {
+    clienteControle.Apagar(cliente.Id);
+    FrameAviso.IsVisible = false;
+    Application.Current.MainPage = new Funcionarios();
+  }
+
+  private void NaoExcluir(object sender, EventArgs e)
+  {
+    FrameAviso.IsVisible = false;
+  }
+  //---------------------------------------------------------------------------------------------------------------------\\
+
+  protected override void OnAppearing()
+  {
+    base.OnAppearing();
+
+    if (cliente != null)
     {
-        InitializeComponent();
-        cliente = new Cliente();
-        clienteControle = new ClienteControle();
+      IdLabel.Text = cliente.Id.ToString();
+      NameEntry.Text = cliente.Nome;
+      HorarioEntry.Text = cliente.Horario;
+      SalarioEntry.Text = cliente.Salario;
+      RGEntry.Text = cliente.RG;
+      CPFEntry.Text = cliente.CPF;
     }
+  }
 
-//---------------------------------------------------------------------------------------------------------------------\\
 
-    private void Apagar(object sender, EventArgs e)
+  //---------------------------------------------------------------------------------------------------------------------\\
+
+  private async void Salvar(object sender, EventArgs e)
+  {
+    if (await VerificaSeDadosEstaoCorretos()) // Verifica se os dados são válidos antes de salvar no banco
     {
-        // Handle the trash button click event
-        NameEntry.Text = string.Empty;
-        CPFEntry.Text = string.Empty;
-        RGEntry.Text = string.Empty;
-        HorarioEntry.Text = string.Empty;
-        SalarioEntry.Text = string.Empty;
+      // O código abaixo preenche o objeto cliente (Modelo) com os dados das Entry's
+
+      if (!String.IsNullOrEmpty(IdLabel.Text))
+        cliente.Id = int.Parse(IdLabel.Text);
+      else
+        cliente.Id = 0;
+      cliente.Nome = NameEntry.Text;
+      cliente.RG = RGEntry.Text;
+      cliente.CPF = CPFEntry.Text;
+      cliente.Horario = HorarioEntry.Text;
+      cliente.Salario = SalarioEntry.Text;
+
+
+      // Com o objeto preenchido enviamos para o controle para criar/atualizar no Banco de Dados
+      clienteControle.CriarOuAtualizar(cliente);
+      // Mostra a mensagem de sucesso
+      await DisplayAlert("Salvar", "Dados salvos com sucesso!", "OK");
+
+      if (cliente.Nome == null || cliente.CPF == null || cliente.RG == null || cliente.Horario == null || cliente.Salario == null)
+      {
+        FrameErro.IsVisible = true;
+      }
+      else
+      {
+        clienteControle.Ler(cliente.Id);
+        Application.Current.MainPage = new Funcionarios();
+      }
 
     }
+  }
 
-//---------------------------------------------------------------------------------------------------------------------\\
+  //---------------------------------------------------------------------------------------------------------------------\\
 
-    private async void Salvar(object sender, EventArgs e)
-    {
-        if (await VerificaSeDadosEstaoCorretos()) // Verifica se os dados são válidos antes de salvar no banco
-        {
-            // O código abaixo preenche o objeto cliente (Modelo) com os dados das Entry's
-            
-            if (!String.IsNullOrEmpty(IdLabel.Text))
-                cliente.Id = int.Parse(IdLabel.Text);
-            else
-                cliente.Id = 0;
-            cliente.Nome = NameEntry.Text;
-            cliente.RG = RGEntry.Text;
-            cliente.CPF = CPFEntry.Text;
-            cliente.Horario = HorarioEntry.Text;
-            cliente.Salario = SalarioEntry.Text;
-
-
-            // Com o objeto preenchido enviamos para o controle para criar/atualizar no Banco de Dados
-            clienteControle.CriarOuAtualizar(cliente);
-            // Mostra a mensagem de sucesso
-            await DisplayAlert("Salvar", "Dados salvos com sucesso!", "OK");
-
-            if (cliente.Nome == null || cliente.CPF == null || cliente.RG == null || cliente.Horario == null || cliente.Salario == null)
-        {
-            FrameErro.IsVisible = true;
-        }
-        else
-        {
-            clienteControle.Ler(cliente.Id);
-            Application.Current.MainPage = new Funcionarios();
-        }
-
-        }
-    }
-
-    //---------------------------------------------------------------------------------------------------------------------\\
-
-    private async Task<bool> VerificaSeDadosEstaoCorretos()
-    {
-        // Verifica se a Entry do Nome está vazia
+  private async Task<bool> VerificaSeDadosEstaoCorretos()
+  {
+    // Verifica se a Entry do Nome está vazia
     if (String.IsNullOrEmpty(NameEntry.Text))
     {
       await DisplayAlert("Cadastrar", "O campo Nome é obrigatório", "OK");
@@ -99,55 +133,40 @@ public partial class CadastroFuncionarios : ContentPage
     }
     else
       return true;
-    }
-
-//---------------------------------------------------------------------------------------------------------------------\\
-
-    protected override void OnAppearing()
-  {
-    base.OnAppearing();
-
-    if (cliente != null)
-    {
-      IdLabel.Text        = cliente.Id.ToString();
-      NameEntry.Text      = cliente.Nome;
-      HorarioEntry.Text = cliente.Horario;
-      SalarioEntry.Text  = cliente.Salario;
-    }
   }
 
-//---------------------------------------------------------------------------------------------------------------------\\
-   // public void Salvar(object sender, EventArgs e)
-  //  {
-   //     cliente.Nome = NameEntry.Text;
-    //    cliente.CPF = CPFEntry.Text;
-    //    cliente.RG = RGEntry.Text;
-    //    cliente.Horario = HorarioEntry.Text;
-     //   cliente.Salario = SalarioEntry.Text;
-//
-    //    if (cliente.Nome == null || cliente.CPF == null || cliente.RG == null || cliente.Horario == null || cliente.Salario == null)
-     //   {
-    //        FrameErro.IsVisible = true;
-     //   }
-     //   else
-     //   {
-            
-      //      clienteControle.CriarOuAtualizar(cliente);
-      //      clienteControle.Ler(cliente.Id);
-       //     Application.Current.MainPage = new Funcionarios();
-     //   }
-   // }
+  //---------------------------------------------------------------------------------------------------------------------\\
+  /* public void Salvar(object sender, EventArgs e)
+   {
+       cliente.Nome = NameEntry.Text;
+       cliente.CPF = CPFEntry.Text;
+       cliente.RG = RGEntry.Text;
+       cliente.Horario = HorarioEntry.Text;
+       cliente.Salario = SalarioEntry.Text;
 
-//---------------------------------------------------------------------------------------------------------------------\\
+       if (cliente.Nome == null || cliente.CPF == null || cliente.RG == null || cliente.Horario == null || cliente.Salario == null)
+       {
+           FrameErro.IsVisible = true;
+       }
+       else
+       {
 
-    private void TentarDeNovo(object sender, EventArgs e)
-    {
-        FrameErro.IsVisible = false;
-    }
+           clienteControle.CriarOuAtualizar(cliente);
+           clienteControle.Ler(cliente.Id);
+           Application.Current.MainPage = new Funcionarios();
+       }
+   }*/
 
-    private void Voltar(object sender, EventArgs e)
-    {
-        Application.Current.MainPage = new Menu();
-    }
+  //---------------------------------------------------------------------------------------------------------------------\\
+
+  private void TentarDeNovo(object sender, EventArgs e)
+  {
+    FrameErro.IsVisible = false;
+  }
+
+  private void Voltar(object sender, EventArgs e)
+  {
+    Application.Current.MainPage = new Menu();
+  }
 }
 
--- ProjetoAdiministracao/Funcionarios.xaml.cs
+++ ProjetoAdiministracao/Funcionarios.xaml.cs
@@ -15,49 +15,15 @@ public partial class Funcionarios : ContentPage
         cf = new CadastroFuncionarios();
         cliente = new Cliente();
         clienteControle = new ClienteControle();
+        ListaClientes.ItemsSource = clienteControle.LerTodos();
     }
 
-   /* private void Apagar(object sender, EventArgs e)
-    {
-        NomeLabel.Text = string.Empty;
-        CPFLabel.Text = string.Empty;
-        RGLabel.Text = string.Empty;
-        HorarioLabel.Text = string.Empty;
-        SalarioLabel.Text = string.Empty;
-    } */
-
-   /* public void Registrar(object sender, EventArgs e)
-    {
-        clienteControle.Ler(cliente.Id);
-
-
-        IdLabel.Text = cliente.Id.ToString();
-        cliente.Nome = NomeLabel.Text;
-        cliente.CPF = CPFLabel.Text;
-        cliente.RG = RGLabel.Text;
-        cliente.Horario = HorarioLabel.Text;
-        cliente.Salario = SalarioLabel.Text;
-
-
-    }*/
-
-    private void IrCadastroFuncionario(object sender, EventArgs e)
+    private void SelecionarNaTelaFuncionarios(object sender, EventArgs e)
     {
         Application.Current.MainPage = new CadastroFuncionarios();
     }
 //-----------------------------Comprender o do professor-----------------------\\
-   /* protected override void OnAppearing()
-  {
-    base.OnAppearing();
-
-    if (cliente != null)
-    {
-      IdLabel.Text        = cliente.Id.ToString();
-      NomeLabel.Text      = cliente.Nome;
-      HorarioLabel.Text = cliente.Horario;
-      SalarioLabel.Text  = cliente.Salario;
-    }
-  }*/
+  
 
     void QuandoSelecionarUmItemNaLista(object sender, SelectedItemChangedEventArgs e)
     {
